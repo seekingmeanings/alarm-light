5c80133b5fe1a02f00b63c27a9a36a542d9f020f
seekingmeanings <84940126+seekingmeanings@users.noreply.github.com>
2023-01-30 08:28:14 +0100

    save state

--- server.py
+++ server.py
@@ -1,20 +1,16 @@
 import socket as sc
 from netifaces import interfaces, ifaddresses, AF_INET #interfaces not needed?
-
 import time
 
-#from aiy.pins import PIN_A
-#from gpiozero import LED
+from aiy.pins import PIN_A
+from gpiozero import LED
+
 
 
 from pprint import pprint as pp
 
 
 def test_server(addr):
-    with sc.socket(sc.AF_INET, sc.SOCK_STREAM) as active_sct:
-        active_sct.bind(addr)
-        active_sct.listen()
-
         conn, addr = active_sct.accept()
 
         with conn:
@@ -29,13 +25,22 @@ def test_server(addr):
 
 
 
-
-
-
-
-
 # local binding address and port
-#PORT = 40753
-#IP_HOST = ifaddresses("wlan0").setdefault(AF_INET, [{'addr':None}])[0]['addr']
+PORT = 40753
+IP_HOST = ifaddresses("wlan0").setdefault(AF_INET, [{'addr':None}])[0]['addr']
 
-test_server(('localhost', 20876))
+
+asct = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
+asct.bind((IP_HOST, PORT))
+
+asct.listen()
+
+while True:
+    c, addr = asct.accept()
+
+    with c:
+        while True:
+            d = list(c.recv(1024))
+            if not d:
+                break
+            elif 
