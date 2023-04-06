a76a90ec2916bd0ff2226defee844e407aac8ec1
seekingmeanings <git-j@mail.o-family.de>
2023-04-06 22:03:20 +0200

    removed unnessesary code

--- client.py
+++ client.py
@@ -7,13 +7,9 @@ import json
 import time
 
 from lib.daemon import Daemon
-
-import sys
-sys.path.append('/data/data/com.termux/files/home/lib')
-from local_utils import toast
+from lib.utils import termux_api_toast as toast
 
 
-NID = 56
 WORK_DIR = "/data/data/com.termux/files/home/alarm-light"
 
 
@@ -38,31 +34,28 @@ class AlarmDaemon(Daemon):
         super().__init__()
         self.alarm_buffer = []
 
-        self.NID = 56
+        self.nid = 56
         self.alarm_flags = {"packageName": "com.google.android.deskclock",
                             "title": "Upcoming alarm", "group": 1}
 
     def run(self):
         pass
 
+    def stop(self):
+        super().stop()
+
     def find_notifications(self):
         with sp.run(["termux-notification-list"], shell=True,
                     capture_output=True, timeout=5, text=True,
                     check=True) as opt:
 
-            notifications = json.loads("".join([str(line.decode('utf-8')
-                                                    .replace('\n', ''))
-                                                for line in opt.stdout]))
-        # nots = list()
-        # for n in notifications:
-        #    if n['packageName'] == "com.google.android.deskclock"\
-        #       and n['title'] == "Upcoming alarm" and int(n['group']) == 1:
-        #        nots.append(n)
+            notifications = json.loads("".join(
+                [str(line.decode('utf-8').replace('\n', ''))
+                 for line in opt.stdout]))
 
-        return [n if (self.alarm_flags[flag] == n[flag]
-                      for flag in self.alarm_flags)
-                else None
-                for n in notifications]  # remove all occurencys of None
+        return [n if all(self.alarm_flags[flag] == n[flag]
+                         for flag in self.alarm_flags)
+                else None for n in notifications]
 
     def collect_upcoming_alarms(self, alarm_notifications):
         """ example notification
@@ -83,9 +76,8 @@ class AlarmDaemon(Daemon):
             alarm_notifications['content'].join(''), '%a %H:%M'))
         toast(f"alarm found: {self.alarm_buffer[-1]}")
 
-    @staticmethod
-    def notification_switch():  # add last updated as content
-        sp.run(["termux-notification", "-i", str(NID),
+    def notification_switch(self):  # add last updated as content
+        sp.run(["termux-notification", "-i", str(self.nid),
                 "--ongoing", "--button1", "on", "--button1-action",
                 f"python3 {WORK_DIR}/pre_alarm_daemon.py --on",
                 "--button2", "off", "--button2-action",
@@ -94,9 +86,8 @@ class AlarmDaemon(Daemon):
                 f"python3 {WORK_DIR}/pre_alarm_daemon.py -d -k",
                 "-t", "bed light"], check=True)
 
-    @staticmethod
-    def kill_notification(nid):
-        sp.run("termux-notification-remove", str(nid))
+    def kill_notification(self):
+        sp.run("termux-notification-remove", str(self.nid))
         # kill the daemon
 
 
@@ -105,18 +96,16 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--daemon", action="store_true")
     parser.add_argument("-k", "--kill", action="store_true")
 
-    parser.add_argument("-f", "--off", action="store_true")
-    parser.add_argument("-t", "--on", action="store_true")
+    parser.add_argument("--off", action="store_true")
+    parser.add_argument("--on", action="store_true")
 
     args = parser.parse_args()
-
-    # make startup check; use groups instead of nid?
 
     daemon = AlarmDaemon()
 
     if args.daemon:
         if args.kill:
-            daemon.kill_notification(NID)
+            daemon.stop()
         else:
             daemon.notification_switch()
             # loop???
